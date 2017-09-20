5fc20a5ba85dd33d4e8ca5e63fb203d85437688f
SilkeHenderickx <silke.henderickx@student.ap.be>
2017-09-20 14:35:48 +0200

    added promises to half the code for readability

--- HistoricalAchievements.js
+++ HistoricalAchievements.js
@@ -1,72 +1,88 @@
-function getIDHistorical(){
-  $.ajax({
-    headers: {
-      'Accept': 'application/json'
-    },
-    type: "GET",
-    url: "https://api.guildwars2.com/v2/achievements/categories",
-    success: function(data) {
-      var responseArray = [];
-      responseArray.push(data);
-      getAchievementIDHistorical(responseArray)
-    },
-    error: function(err){
-      console.log('error:' + err)
-    },
-    dataType: "json"
+function getIdsOfHistoricalAchievements() {
+  return new Promise(function(resolve){
+    //Collect all category IDS
+    var categoryIdPromise = collectAllCategoryIds();
+    categoryIdPromise.then(function(categoryIDs) {
+      //Collect all historical Category IDS
+      var historicalCategoryIdPromise = collectAllHistoricalCategoryIDS(categoryIDs)
+      historicalCategoryIdPromise.then(function(historicalCatagoryIds) {
+        var historicalAchievementIds = createArrayHistoricalAchievements(historicalCatagoryIds);
+        resolve(historicalAchievementIds);
+      });
+    });
   });
-
-
 }
 
-function getAchievementIDHistorical(categoryIDs){
-  var urlAddon = "";
-  console.log(categoryIDs);
-  $.each(categoryIDs[0], function(index, value){
-    urlAddon = urlAddon + value + ","
+function collectAllHistoricalCategoryIDS(categoryIDs) {
 
+  return new Promise(function(resolve, reject) {
+
+    //build Category Details url
+    var urlAddon = "";
+    console.log(categoryIDs);
+    $.each(categoryIDs[0], function(index, value){
+      urlAddon = urlAddon + value + ","
+    });
+
+    //get full Category API
+    var fullUrl = "https://api.guildwars2.com/v2/achievements/categories?ids=" + urlAddon.slice(0, -1);
+
+    $.ajax({
+      headers: {
+        'Accept': 'application/json'
+      },
+      type: "GET",
+      url: fullUrl ,
+      success: function(fullCategoryAPIResponse) {
+        resolve(fullCategoryAPIResponse);
+      },
+      error: function(err){
+        console.log('error:' + err)
+        reject(err);
+      },
+      dataType: "json",
+
+    });
   });
-  gethistorical(urlAddon);
 }
-
-
-function gethistorical(urlAddon){
-
-var fullUrl = "https://api.guildwars2.com/v2/achievements/categories?ids=" + urlAddon.slice(0, -1);
-
-$.ajax({
-  headers: {
-    'Accept': 'application/json'
-  },
-  type: "GET",
-  url: fullUrl ,
-  success: function(data) {
-    getArrayHistoricalAchievements(data);
-
-  },
-  error: function(err){
-    console.log('error:' + err)
-  },
-  dataType: "json"
-});
-}
-
-function getArrayHistoricalAchievements(data){
-
-var historicalAchievements = [];
+function createArrayHistoricalAchievements(fullCategoryAPIResponse){
+  var data = fullCategoryAPIResponse;
+  var historicalAchievementIds = [];
 
   for (var i = 0; i < data.length; i++) {
     if(data[i].order == 0){
       for (var j = 0; j < data[i].achievements.length; j++) {
-
-
-      if (data[i].achievements[j] != undefined) {
-        historicalAchievements.push(data[i].achievements[j])
+        if (data[i].achievements[j] != undefined) {
+          historicalAchievementIds.push(data[i].achievements[j])
+        }
       }
-
     }
-  }
 
+  }
+  console.log(historicalAchievementIds);
+  return historicalAchievementIds;
 }
-console.log(historicalAchievements);
+
+function collectAllCategoryIds() {
+  return new Promise(function(resolve, reject) {
+    // do a thing, possibly async, then…
+    $.ajax({
+      headers: {
+        'Accept': 'application/json'
+      },
+      type: "GET",
+      url: "https://api.guildwars2.com/v2/achievements/categories",
+      success: function(data) {
+        var categoryIDs = [];
+        categoryIDs.push(data);
+        resolve(categoryIDs)
+        // REPLACED BY PROMISE ABOVE getAchievementIDHistorical(responseArray)
+      },
+      error: function(err){
+        console.log('error:' + err)
+        reject(err)
+      },
+      dataType: "json"
+    });
+  });
 }
